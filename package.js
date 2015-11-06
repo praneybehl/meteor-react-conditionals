cc6425e9f982fcd6a41f6213246edba4149f144f
Praney Behl <praneybehl@gmail.com>
2015-11-06 22:46:24 +1100

    update package dependencies

--- package.js
+++ package.js
@@ -1,24 +1,23 @@
 Package.describe({
   name: 'praneybehl:react-conditionals',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '0.1.5',
+  summary: 'React component for easy conditional logic packaged for Meteor',
+  git: 'https://github.com/praneybehl/meteor-react-conditionals',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('react-conditional.js');
+  api.use('react@0.1.13');
+  api.use('cosmos:browserify@0.8.3', 'client');
+  api.addFiles('package.browserify.js', 'client');
+  api.addFiles('package.browserify.options.json', 'client');
+  api.export('Conditional');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('praneybehl:react-conditional');
-  api.addFiles('react-conditional-tests.js');
+
+Npm.depends({
+  "react-conditional-component": "0.1.5",
+  "exposify": "0.5.0"
 });
